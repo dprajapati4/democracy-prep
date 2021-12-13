f51843719f9ca5b9550929dd645c77ed5b875e4a
Deep Prajapati <dprajap2@gmail.com>
2021-12-12 20:22:09 -0500

    Chore: Removes redux and sets up react-router

---
+++ client/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4> Hi! Thanks for coming to our website. Please take a look at our campuses and students.</h4>
+      
+      </div>
+    );
+  }
+}
+export default Home;
--- client/index.js
+++ client/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import store from './store'
-import '../public/style.css'
+import '../public/style.css';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 ReactDOM.render(
-  <Provider store={store}>
-  <div>Hello, world!</div>
-  </Provider>,
-  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+  <div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Routes>
+            <Route exact path="/" component={<Home />} />
+          </Routes>
+        </main>
+      </div>
+      <h1> Democracy Prep Schools Survey Form</h1>
+    </Router>
+  </div>,
+  document.getElementById('app')
 );
