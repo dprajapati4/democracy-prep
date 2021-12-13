052cc537d1e9dbfadd70e4563fef807aefaeeca3
Deep Prajapati <dprajap2@gmail.com>
2021-12-12 20:37:58 -0500

    Style: Add basic styling and google font

--- client/index.js
+++ client/index.js
@@ -9,6 +9,11 @@ ReactDOM.render(
     <Router>
       <div>
         <nav>
+          <div id="logo">
+            <a href="">
+              <img src="https://democracyprep.org/wp-content/themes/dpps/images/header-logo@2x.png" width="221" height="48" alt="democracy prep" />
+            </a>
+          </div>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -21,7 +26,7 @@ ReactDOM.render(
           </Routes>
         </main>
       </div>
-      <h1> Democracy Prep Schools Survey Form</h1>
+      <h1> Democracy Prep Schools Survey Form </h1>
     </Router>
   </div>,
   document.getElementById('app')
